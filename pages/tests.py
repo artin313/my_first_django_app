e0f94409641366e3c52f74fb557dc5c230437671
Artin.Dervishi <artin.1415@gmail.com>
2023-07-04 20:39:32 +0200

    Added tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class homePageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+    def test_url_availabe_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code,200)
+
+    def test_template_dontent(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response,'<h2>diçka per Mitrovicen? a?</h2>')
